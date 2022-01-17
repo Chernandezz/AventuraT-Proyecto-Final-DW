39bd0b46bab1178bb351efea7d7f7d774cc58796
Crisitian Felipe Hernandez Zuluaga <chernandezz@unal.edu.co>
2022-01-17 16:39:10 -0500

    Se finalizo de hacer el header responsive y con todo lo que comprende, ya de empezo a trabajar en la pagina de inicio

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let navbar = document.querySelector(".header .navegacion");
+let iconos = document.querySelector(".iconos");
+
+document.querySelector("#btn-menu").onclick = () => {
+  navbar.classList.add("activo");
+  iconos.classList.add("oculto");
+};
+
+document.querySelector("#cerrar-nav").onclick = () => {
+  navbar.classList.remove("activo");
+  iconos.classList.remove("oculto");
+};
+
+let buscador = document.querySelector(".buscador");
+
+document.querySelector("#btn-buscar").onclick = () => {
+  buscador.classList.add("activo");
+  iconos.classList.add("oculto");
+};
+
+document.querySelector("#cerrar-buscador").onclick = () => {
+  buscador.classList.remove("activo");
+  iconos.classList.remove("oculto");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("activo");
+};
