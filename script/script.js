f1bb4c5a5e4ff7f6efe4fae74fb81f80f58087ed
Crisitian Felipe Hernandez Zuluaga <chernandezz@unal.edu.co>
2022-01-19 08:12:43 -0500

    Se termino la seccion de la tienda con el slider y todo funcionando muy bien, se va a comenzar a trabajar en los paquetes

--- script/script.js
+++ script/script.js
@@ -49,3 +49,27 @@ var swiper = new Swiper(".slider-inicio", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".slider-productos", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
