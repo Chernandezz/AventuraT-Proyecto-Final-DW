a58cf4f89bc4bb05ced1adc4684e46e8af4e9036
Crisitian Felipe Hernandez Zuluaga <chernandezz@unal.edu.co>
2022-02-02 20:10:18 -0500

    Se Termino el Create de productos
    Se termino el Create del CRUD de productos con la respectiva validacion y seguridad del mismo, tambien se creo un codigo especial para almacenar las imagenes y que no choque el nombre

--- admin/productos/crear.php
+++ admin/productos/crear.php
@@ -2,10 +2,66 @@
 require "../../includes/config/database.php";
 $db = conectarDB();
 
+$errores = [];
+
+$titulo = "";
+$precio = "";
+$descripcion = "";
+$cantidad = "";
+
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    echo "<pre>";
-    var_dump($_POST['titulo']);
-    echo "</pre>";
+    $titulo = mysqli_real_escape_string($db, $_POST['titulo']);
+    $precio = mysqli_real_escape_string($db, $_POST['precio']);
+    $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+    $cantidad = mysqli_real_escape_string($db, $_POST['cantidad']);
+
+    // Asignar super globlar $_FILES a una variable
+    $imagen = $_FILES['imagen'];
+
+    if (!$titulo) {
+        $errores[] = 'el titulo es requerido';
+    }
+    if (!$precio) {
+        $errores[] = 'el precio es requerido';
+    }
+    if (!$cantidad) {
+        $errores[] = 'la cantidad es requerida';
+    }
+
+    // Validacion Imagen
+    if (!$imagen['name'] || $imagen['error']) {
+        $errores[] = 'la imagen es requerida';
+    }
+
+    //  Revisar que el arreglo de errores este vacio
+
+    if (empty($errores)) {
+
+        // Subida de archivos
+
+        // Crear Carpeta
+
+        $carpetaImagenes = '../../imagenes/';
+        if (!is_dir($carpetaImagenes)) {
+            mkdir($carpetaImagenes);
+        }
+
+        // Generar un nombre unico
+        $nombreImagen = md5(uniqid('')) . '.png';
+
+        // Subir Imagen
+        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen);
+
+        // Insertar en la base de datos
+        $query = "INSERT INTO productos (titulo, precio, imagen, descripcion, cantidad) VALUES ('$titulo', '$precio', '$nombreImagen', '$descripcion', '$cantidad');";
+
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            // Redireccionar al Usuario
+            header("Location: /admin?accion=1");
+        }
+    }
 
 }
 
@@ -18,16 +74,22 @@ incluirTemplate('header');
 
     <h2 class="encabezadoCRUD">nuevo producto</h2>
 
-    <form class="formulario formCRUD" method="POST" enctype="">
+    <?php foreach ($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach;?>
+
+    <form class="formulario formCRUD" method="POST" enctype="multipart/form-data">
         <fieldset>
                 <div class="caja">
                     <label for="titulo">titulo</label>
-                    <input type="text" name="titulo" id="titulo" placeholder="titulo producto">
+                    <input type="text" name="titulo" id="titulo" placeholder="titulo producto" value="<?php echo $titulo ?>">
                 </div>
 
                 <div class="caja">
                     <label for="precio">precio</label>
-                    <input type="number" name="precio" id="precio" placeholder="precio producto">
+                    <input type="number" name="precio" id="precio" placeholder="precio producto" value="<?php echo $precio ?>">
                 </div>
 
                 <div class="caja">
@@ -37,12 +99,12 @@ incluirTemplate('header');
 
                 <div class="caja">
                     <label for="descripcion">descripción</label>
-                    <textarea name="descripcion" id="descripcion"></textarea>
+                    <textarea name="descripcion" id="descripcion"><?php echo $descripcion ?></textarea>
                 </div>
 
                 <div class="caja">
                     <label for="cantidad">cantidad</label>
-                    <input type="number" name="cantidad" id="cantidad" placeholder="cantidad disponible" min="1">
+                    <input type="number" name="cantidad" id="cantidad" placeholder="cantidad disponible" min="1" value="<?php echo $cantidad ?>">
                 </div>
         </fieldset>
         <input type="submit" value="crear producto" class="btn">
