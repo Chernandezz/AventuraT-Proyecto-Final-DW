22e7bcf9a707300d9f2499f898d9253d3ae3d126
Cristian Hernandez <chernandezz@unal.edu.co>
2023-01-04 20:40:38 -0500

    Actualizaciones a la conexion con la BD

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB(): mysqli
 {
-    $db = mysqli_connect('localhost', 'root', '', 'aventurat');
+    $db = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b061e83da8b7cc', 'd8a97d8b', 'heroku_bd417071f19136c');
 
     if (!$db) {
         echo "Error, no se pudo conectar a la BD";
@@ -11,3 +11,13 @@ function conectarDB(): mysqli
 
     return $db;
 }
+/*
+mysql://b061e83da8b7cc:d8a97d8b@us-cdbr-east-06.cleardb.net/heroku_bd417071f19136c?reconnect=true
+
+
+Username: b061e83da8b7cc
+Password: d8a97d8b
+Server: us-cdbr-east-06.cleardb.net
+Port: 3306
+Database: heroku_bd417071f19136c
+*/
